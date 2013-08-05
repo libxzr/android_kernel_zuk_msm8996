bdec7455f7174dc88e4fbe51030a91ecb3bac181
Shantanu Jain <shjain@codeaurora.org>
2013-08-05 16:52:47 +0530

    input: touchscreen: release all touches before suspend
    release all touches before going to suspend for goodix
    gt9xx TS driver.
    
    This patch is propagated from msm-3.4 kernel
    (commit: a1b5c4559026d9ddc789f64e17c1ade0a281d372
    input: touchscreen: release all touches before suspend)
    
    Change-Id: Ia3446f3c23be4fe29bfea5f85b22e8b903cffb9e
    Signed-off-by: Shantanu Jain <shjain@codeaurora.org>
    Signed-off-by: Sudhakar Manapati <smanap@codeaurora.org>
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- drivers/input/touchscreen/gt9x_2.4/gt9xx.c
+++ drivers/input/touchscreen/gt9x_2.4/gt9xx.c
@@ -2016,8 +2016,8 @@ Output:
 *******************************************************/
 static void goodix_ts_suspend(struct goodix_ts_data *ts)
 {
-	s8 ret = -1;    
-	
+	int ret = -1, i;    
+
 	GTP_DEBUG_FUNC();
 	if(ts->gtp_is_suspend)
 		return;
@@ -2025,7 +2025,7 @@ static void goodix_ts_suspend(struct goodix_ts_data *ts)
 		return;
 	}
 	GTP_INFO("System suspend.");
-	
+
 
 	ts->gtp_is_suspend = 1;
 #if GTP_ESD_PROTECT
@@ -2061,7 +2061,13 @@ static void goodix_ts_suspend(struct goodix_ts_data *ts)
 	else
 	{
 		hrtimer_cancel(&ts->timer);
-	}
+
+	for (i = 0; i < GTP_MAX_TOUCH; i++)
+		gtp_touch_up(ts, i);
+
+ 	input_report_key(ts->input_dev, BTN_TOUCH, 0);
+	input_sync(ts->input_dev);
+
 	ret = gtp_enter_sleep(ts);
 	gtp_power_switch(ts->client, 0);
 	ts->ts_pinctrl = devm_pinctrl_get(&ts->client->dev);
