d989c353292699367c4de876e63a9171f0a71e8c
Olav Haugan <ohaugan@codeaurora.org>
2015-08-05 08:45:21 -0700

    sched: Update task->on_rq when tasks are moving between runqueues
    Task->on_rq has three states:
    	0 - Task is not on runqueue (rq)
    	1 (TASK_ON_RQ_QUEUED) - Task is on rq
    	2 (TASK_ON_RQ_MIGRATING) - Task is on rq but in the
    	process of being migrated to another rq
    
    When a task is moving between rqs task->on_rq state should be
    TASK_ON_RQ_MIGRATING in order for WALT to account rq's cumulative
    runnable average correctly.  Without such state marking for all the
    classes, WALT's update_history() would try to fixup task's demand
    which was never contributed to any of CPUs during migration.
    
    Change-Id: Iced3428f3924fe8ab5d0075698273ead04f12d5b
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>
    [joonwoop: Reinforced changelog to explain why this is needed by WALT.
               Fixed conflicts in deadline.c]
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -1407,7 +1407,9 @@ retry:
 
 	deactivate_task(rq, next_task, 0);
 	clear_average_bw(&next_task->dl, &rq->dl);
+	next_task->on_rq = TASK_ON_RQ_MIGRATING;
 	set_task_cpu(next_task, later_rq->cpu);
+	next_task->on_rq = TASK_ON_RQ_QUEUED;
 	add_average_bw(&next_task->dl, &later_rq->dl);
 	activate_task(later_rq, next_task, 0);
 
@@ -1495,7 +1497,9 @@ static int pull_dl_task(struct rq *this_rq)
 
 			deactivate_task(src_rq, p, 0);
 			clear_average_bw(&p->dl, &src_rq->dl);
+			p->on_rq = TASK_ON_RQ_MIGRATING;
 			set_task_cpu(p, this_cpu);
+			p->on_rq = TASK_ON_RQ_QUEUED;
 			add_average_bw(&p->dl, &this_rq->dl);
 			activate_task(this_rq, p, 0);
 			dmin = p->dl.deadline;
