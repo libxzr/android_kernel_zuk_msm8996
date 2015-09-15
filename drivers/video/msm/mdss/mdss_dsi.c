f3c5be04fd8d6bdf9249fcf1eb52b7011eb0b509
Se Hun Kim <sean2.kim@lge.com>
2015-09-15 15:11:20 +0200

    msm: mdss: Adding lcd notifier
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -23,6 +23,7 @@
 #include <linux/regulator/consumer.h>
 #include <linux/leds-qpnp-wled.h>
 #include <linux/clk.h>
+#include <linux/lcd_notify.h>
 #include <linux/uaccess.h>
 #include <linux/msm-bus.h>
 #include <linux/pm_qos.h>
@@ -2651,6 +2652,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 							pdata);
 		break;
 	case MDSS_EVENT_UNBLANK:
+		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
 		if (ctrl_pdata->on_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_unblank(pdata);
 		break;
@@ -2665,8 +2667,10 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		#ifdef CONFIG_STATE_NOTIFIER
 		state_resume();
 		#endif
+		lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
 		break;
 	case MDSS_EVENT_BLANK:
+		lcd_notifier_call_chain(LCD_EVENT_OFF_START, NULL);
 		power_state = (int) (unsigned long) arg;
 		if (ctrl_pdata->off_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_blank(pdata, power_state);
@@ -2681,6 +2685,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		#ifdef CONFIG_STATE_NOTIFIER
 		state_suspend();
 		#endif
+		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
