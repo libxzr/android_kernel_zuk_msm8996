1fbfb7f050817cf51d533ac1f44656520c9892ab
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2016-05-31 09:08:38 -0700

    sched: Introduce Window Assisted Load Tracking (WALT)
    use a window based view of time in order to track task
    demand and CPU utilization in the scheduler.
    
    Window Assisted Load Tracking (WALT) implementation credits:
     Srivatsa Vaddagiri, Steve Muckle, Syed Rameez Mustafa, Joonwoo Park,
     Pavan Kumar Kondeti, Olav Haugan
    
    2016-03-06: Integration with EAS/refactoring by Vikram Mulukutla
                and Todd Kjos
    
    Change-Id: I21408236836625d4e7d7de1843d20ed5ff36c708
    
    Includes fixes for issues:
    
    eas/walt: Use walt_ktime_clock() instead of ktime_get_ns() to avoid a
    race resulting in watchdog resets
    BUG: 29353986
    Change-Id: Ic1820e22a136f7c7ebd6f42e15f14d470f6bbbdb
    
    Handle walt accounting anomoly during resume
    
    During resume, there is a corner case where on wakeup, a task's
    prev_runnable_sum can go negative. This is a workaround that
    fixes the condition and warns (instead of crashing).
    
    BUG: 29464099
    Change-Id: I173e7874324b31a3584435530281708145773508
    
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Signed-off-by: Srinath Sridharan <srinathsr@google.com>
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    [jstultz: fwdported to 4.4]
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: joshuous <joshuous@gmail.com>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -7,6 +7,8 @@
 
 #include <linux/slab.h>
 
+#include "walt.h"
+
 int sched_rr_timeslice = RR_TIMESLICE;
 
 static int do_sched_rt_period_timer(struct rt_bandwidth *rt_b, int overrun);
@@ -1328,6 +1330,7 @@ enqueue_task_rt(struct rq *rq, struct task_struct *p, int flags)
 		rt_se->timeout = 0;
 
 	enqueue_rt_entity(rt_se, flags);
+	walt_inc_cumulative_runnable_avg(rq, p);
 
 	if (!task_current(rq, p) && p->nr_cpus_allowed > 1)
 		enqueue_pushable_task(rq, p);
@@ -1339,6 +1342,7 @@ static void dequeue_task_rt(struct rq *rq, struct task_struct *p, int flags)
 
 	update_curr_rt(rq);
 	dequeue_rt_entity(rt_se, flags);
+	walt_dec_cumulative_runnable_avg(rq, p);
 
 	dequeue_pushable_task(rq, p);
 }
