742e885baea940685674df41d6e35668ba544075
Anirudh Ghayal <aghayal@codeaurora.org>
2016-08-04 08:12:10 +0530

    power: power_supply: Add property to limit pulses for HVDCP
    Use the property POWER_SUPPLY_PROP_MAX_PULSE_ALLOWED to
    specify the maximum pulses which can be supported for
    HVDCP3. This is used to limit the VBUS to a max value.
    
    CRs-Fixed: 1047232
    Change-Id: I85677ba574ff3da2a38e06a2f25a43e80ae150d6
    Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>
    Signed-off-by: Vamshi Krishna B V <vbv@codeaurora.org>
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -262,6 +262,7 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_SDP_CURRENT_MAX,
 	POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE,
 	POWER_SUPPLY_PROP_ALLOW_HVDCP3,
+	POWER_SUPPLY_PROP_MAX_PULSE_ALLOWED,
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_PROP_CHARGE_COUNTER_EXT,
 	/* Properties of type `const char *' */
