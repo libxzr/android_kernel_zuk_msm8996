0b6af1739214b9f6ab1e801ab36061586e25bb15
John Stultz <john.stultz@linaro.org>
2016-09-20 18:42:22 -0700

    sched: Add Kconfig option DEFAULT_USE_ENERGY_AWARE to set ENERGY_AWARE feature flag
    The ENERGY_AWARE sched feature flag cannot be set unless
    CONFIG_SCHED_DEBUG is enabled.
    
    So this patch allows the flag to default to true at build time
    if the config is set.
    
    Change-Id: I8835a571fdb7a8f8ee6a54af1e11a69f3b5ce8e6
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -83,4 +83,8 @@ SCHED_FEAT(NUMA_RESIST_LOWER, false)
  * Energy aware scheduling. Use platform energy model to guide scheduling
  * decisions optimizing for energy efficiency.
  */
+#ifdef CONFIG_DEFAULT_USE_ENERGY_AWARE
+SCHED_FEAT(ENERGY_AWARE, true)
+#else
 SCHED_FEAT(ENERGY_AWARE, false)
+#endif
