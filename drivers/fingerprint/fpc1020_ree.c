ce19c45177f93220e287f54b3c24be2099ec5749
Álvaro Brey <alvaro.brv@gmail.com>
2017-03-16 13:19:54 +0100

    drivers: fpc/gpio cleanup
    * General improvements to code style
    * Remove unused functions
    * Remove redundant param for reset_home_button
    * Reuse function calls as variables to improve performance
    * Simplify boolean checks
    * Document extern functions and relevant variables
    
    Change-Id: I597c5d4f35b80b797d6ab51dc0030d48d665bad1
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- drivers/fingerprint/fpc1020_ree.c
+++ drivers/fingerprint/fpc1020_ree.c
@@ -57,8 +57,14 @@ struct fpc1020_data {
 	int screen_on;
 };
 
+/*
+ * From drivers/input/keyboard/gpio_keys.c
+ */
 extern bool home_button_pressed(void);
-extern void reset_home_button(bool);
+/*
+ * From drivers/input/keyboard/gpio_keys.c
+ */
+extern void reset_home_button(void);
 
 bool reset;
 
@@ -86,7 +92,7 @@ static ssize_t irq_set(struct device* device,
 	else if (val == 0)
 		disable_irq(fpc1020->irq);
 	else
-		return -ENOENT; 
+		return -ENOENT;
 	return strnlen(buffer, count);
 }
 
@@ -156,7 +162,7 @@ static DEVICE_ATTR(wakeup, S_IRUSR | S_IWUSR, get_wakeup_status, set_wakeup_stat
 static ssize_t get_key(struct device* device, struct device_attribute* attribute, char* buffer)
 {
 	struct fpc1020_data* fpc1020 = dev_get_drvdata(device);
-		return scnprintf(buffer, PAGE_SIZE, "%i\n", fpc1020->report_key);
+	return scnprintf(buffer, PAGE_SIZE, "%i\n", fpc1020->report_key);
 }
 
 static ssize_t set_key(struct device* device,
@@ -166,29 +172,28 @@ static ssize_t set_key(struct device* device,
 	int retval = 0;
 	u64 val;
 	struct fpc1020_data* fpc1020 = dev_get_drvdata(device);
+	bool home_pressed;
 
 	retval = kstrtou64(buffer, 0, &val);
 	if (!retval) {
 		if (val == KEY_HOME)
 			val = KEY_NAVI_LONG;  //Convert to U-touch long press keyValue
-		if (val != 0 && home_button_pressed()) val = 0;
 
-		pr_info("home key pressed = %d\n", (int)home_button_pressed());
+	    home_pressed = home_button_pressed();
+
+		if (val && home_pressed) val = 0;
+
+		pr_info("home key pressed = %d\n", (int)home_pressed);
 		fpc1020->report_key = (int)val;
 		queue_work(fpc1020->fpc1020_wq, &fpc1020->input_report_work);
 
-		if (val == 0) {
+		if (!val) {
 			pr_info("calling home key reset");
-			reset_home_button(0);
+			reset_home_button();
 		}
 	} else
 		return -ENOENT;
 	return strnlen(buffer, count);
-}
-
-bool reset_gpio(void)
-{
-	return reset;
 }
 
 static DEVICE_ATTR(key, S_IRUSR | S_IWUSR, get_key, set_key);
