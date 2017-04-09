6d8292917ee4e6a32b9690ce99c35ae49526100f
flar2 <asegaert@gmail.com>
2017-04-10 01:31:07 +0530

    Disable Touch-Boost and Expose Toggle to sysFS
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -31,6 +31,7 @@
 static unsigned int use_input_evts_with_hi_slvt_detect;
 static struct mutex managed_cpus_lock;
 
+static int touchboost = 0;
 
 /* Maximum number to clusters that this module will manage*/
 static unsigned int num_clusters;
@@ -193,6 +194,29 @@ static struct input_handler *handler;
 
 
 /**************************sysfs start********************************/
+
+static int set_touchboost(const char *buf, const struct kernel_param *kp)
+{
+	int val;
+
+	if (sscanf(buf, "%d\n", &val) != 1)
+		return -EINVAL;
+
+	touchboost = val;
+
+	return 0;
+}
+
+static int get_touchboost(char *buf, const struct kernel_param *kp)
+{
+	return snprintf(buf, PAGE_SIZE, "%d", touchboost);
+}
+
+static const struct kernel_param_ops param_ops_touchboost = {
+	.set = set_touchboost,
+	.get = get_touchboost,
+};
+device_param_cb(touchboost, &param_ops_touchboost, NULL, 0644);
 
 static int set_num_clusters(const char *buf, const struct kernel_param *kp)
 {
@@ -385,6 +409,11 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	cpumask_var_t limit_mask;
 	int ret;
 
+	const char *reset = "0:0 4:0";
+
+	if (touchboost == 0)
+		cp = reset;
+
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
 
@@ -392,7 +421,11 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	if (!(ntokens % 2))
 		return -EINVAL;
 
-	cp = buf;
+	if (touchboost == 0)
+		cp = reset;
+	else
+		cp = buf;
+
 	cpumask_clear(limit_mask);
 	for (i = 0; i < ntokens; i += 2) {
 		if (sscanf(cp, "%u:%u", &cpu, &val) != 2)
