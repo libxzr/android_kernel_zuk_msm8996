969474a8c40d4670e17090c97293823a9f2b5618
MOVZX <movzx@yahoo.com>
2017-12-10 22:23:40 +0300

    adreno_tz: Push 27MHz frequency step to be used a bit more
    Signed-off-by: YaroSt12 <yaro330@gmail.com>
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>
    Signed-off-by: mracar07 <mr.acar07@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -397,6 +397,10 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 		scm_data[3] = context_count;
 		__secure_tz_update_entry3(scm_data, sizeof(scm_data),
 					&val, sizeof(val), priv);
+		if ((val == 0) &&
+			(level >= (devfreq->profile->max_state - 2)) &&
+			((priv->bin.busy_time * 100 / priv->bin.total_time) < 98))
+			val = 1;
 	}
 	priv->bin.total_time = 0;
 	priv->bin.busy_time = 0;
