7c375fb4d62f63f0dd2276dc447c0e06a6498001
Davide Garberi <dade.garberi@gmail.com>
2018-02-03 12:25:41 +0100

    sound: msm8996: Disable QUAT_MI2S_ENABLE for z2_plus
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- sound/soc/msm/msm8996.c
+++ sound/soc/msm/msm8996.c
@@ -62,7 +62,9 @@
 #define NCX_VTG_MIN_UV	3000000
 #define NCX_VTG_MAX_UV	3000000
 
+#ifdef CONFIG_MACH_ZUK_Z2_ROW
 #define QUAT_MI2S_ENABLE
+#endif
 #ifdef QUAT_MI2S_ENABLE
 atomic_t quat_mi2s_rsc_ref;
 atomic_t quat_mi2s_clk_ref;
