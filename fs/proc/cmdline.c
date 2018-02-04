0a4a4476981ce85bb8ad8ba4363b6490a7b547d8
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2018-02-04 23:50:25 +0100

    cmdline: Fix offline charging
    * Add androidboot.mode=charger
    * Based on sultanxda/android_kernel_oneplus_msm8996@abc05b16bbd33521c2fffaf491c5657a94bfcfc5
    
    * Fix offline charging with older basebands
    
    * It's androidboot.mode not androidboot.bootmode=usb_chg
    
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    [yaro]: adapted to work with Sultan's SafetyNet patches.
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -48,7 +48,26 @@ static void remove_safetynet_flags(char *cmd)
 
 static int __init proc_cmdline_init(void)
 {
+	char *offset_addr, *cmd = new_command_line;
+
 	strcpy(new_command_line, saved_command_line);
+
+	offset_addr = strstr(cmd, "androidboot.mode=usb_chg");
+	if (offset_addr) {
+		size_t i, len, offset;
+		len = strlen(cmd);
+		offset = offset_addr - cmd;
+		for (i = 1; i < (len - offset); i++) {
+			if (cmd[offset + i] == ' ')
+				break;
+		}
+		memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
+		strcat(cmd, " androidboot.mode=charger");
+	} else {
+		if (strstr(cmd, "androidboot.bootreason=usb_chg")) {
+			strcat(cmd, " androidboot.mode=charger");
+		}
+	}
 
 	/*
 	 * Remove various flags from command line seen by userspace in order to
