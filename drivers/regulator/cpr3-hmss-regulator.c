e953457cb6ec43123ce5573ba673efcf8f0deb55
LibXZR <xzr467706992@163.com>
2018-05-06 10:45:11 +0800

    regulator: Use a much lower voltage for less heat

--- drivers/regulator/cpr3-hmss-regulator.c
+++ drivers/regulator/cpr3-hmss-regulator.c
@@ -366,18 +366,18 @@ enum msm8996_cpr_partial_binning {
 /* Open loop voltage fuse reference voltages in microvolts for MSM8996 v1/v2 */
 static const int msm8996_v1_v2_hmss_fuse_ref_volt[MSM8996_HMSS_FUSE_CORNERS] = {
 	605000,
-	745000, /* Place holder entry for LowSVS */
-	745000,
-	905000,
+	695000, /* Place holder entry for LowSVS */
+	695000,
+	855000,
 	1015000,
 };
 
 /* Open loop voltage fuse reference voltages in microvolts for MSM8996 v3 */
 static const int msm8996_v3_hmss_fuse_ref_volt[MSM8996_HMSS_FUSE_CORNERS] = {
 	605000,
-	745000, /* Place holder entry for LowSVS */
-	745000,
-	905000,
+	695000, /* Place holder entry for LowSVS */
+	695000,
+	855000,
 	1250000,
 };
 
@@ -388,9 +388,9 @@ static const int msm8996_v3_hmss_fuse_ref_volt[MSM8996_HMSS_FUSE_CORNERS] = {
 static const int msm8996_v3_speed_bin1_rev5_hmss_fuse_ref_volt[
 						MSM8996_HMSS_FUSE_CORNERS] = {
 	605000,
-	745000, /* Place holder entry for LowSVS */
-	745000,
-	905000,
+	695000, /* Place holder entry for LowSVS */
+	695000,
+	855000,
 	1040000,
 };
 
