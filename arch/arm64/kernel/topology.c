772817825b4b8860a1b0d07dfab23ae7ddb1410c
joshuous <joshuous@gmail.com>
2018-05-12 23:05:02 +0800

    Revert "arm64: topology: Allow specifying the CPU efficiency from device tree"
    This reverts commit 0b221d8371abcd328d3ad5ba6b3d81badacb679f.

--- arch/arm64/kernel/topology.c
+++ arch/arm64/kernel/topology.c
@@ -280,7 +280,6 @@ static void __init parse_dt_cpu_power(void)
 	for_each_possible_cpu(cpu) {
 		const u32 *rate;
 		int len;
-		u32 efficiency;
 
 		/* Too early to use cpu->of_node */
 		cn = of_get_cpu_node(cpu, NULL);
@@ -289,29 +288,16 @@ static void __init parse_dt_cpu_power(void)
 			continue;
 		}
 
-		/*
-		 * The CPU efficiency value passed from the device tree
-		 * overrides the value defined in the table_efficiency[]
-		 */
-		if (of_property_read_u32(cn, "efficiency", &efficiency) < 0) {
+		for (cpu_eff = table_efficiency; cpu_eff->compatible; cpu_eff++)
+			if (of_device_is_compatible(cn, cpu_eff->compatible))
+				break;
 
-			for (cpu_eff = table_efficiency;
-					cpu_eff->compatible; cpu_eff++)
-
-				if (of_device_is_compatible(cn,
-						cpu_eff->compatible))
-					break;
-
-			if (cpu_eff->compatible == NULL) {
-				pr_warn("%s: Unknown CPU type\n",
-						cn->full_name);
-				continue;
-			}
-
-			efficiency = cpu_eff->efficiency;
+		if (cpu_eff->compatible == NULL) {
+			pr_warn("%s: Unknown CPU type\n", cn->full_name);
+			continue;
 		}
 
-		per_cpu(cpu_efficiency, cpu) = efficiency;
+		per_cpu(cpu_efficiency, cpu) = cpu_eff->efficiency;
 
 		rate = of_get_property(cn, "clock-frequency", &len);
 		if (!rate || len != 4) {
@@ -320,7 +306,7 @@ static void __init parse_dt_cpu_power(void)
 			continue;
 		}
 
-		capacity = ((be32_to_cpup(rate)) >> 20) * efficiency;
+		capacity = ((be32_to_cpup(rate)) >> 20) * cpu_eff->efficiency;
 
 		/* Save min capacity of the system */
 		if (capacity < min_capacity)
