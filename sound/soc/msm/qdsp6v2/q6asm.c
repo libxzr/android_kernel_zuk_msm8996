4617088b74d4c9d498f5019c438574631a41f090
Angelo G. Del Regno <kholk11@gmail.com>
2018-06-13 12:10:29 +0200

    ASoC: msm: qdsp6v2: Force V3 functions for MSM8996
    The drivers are sometimes calling the V4 functions directly for
    some monkey reason...
    This makes the DSP to go completely nuts since the firmware
    is not updated to support the V4 functions.
    
    To address this, even if - okay, it's ugly - add ifdefs to force
    calling the corresponding V3 functions: this makes sure that we
    won't get breakage from random CAF tags upgrades and such.
    
    Signed-off-by: Subhajeet Muhuri <kenny3fcb@gmail.com>
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -2957,9 +2957,13 @@ EXPORT_SYMBOL(q6asm_open_read_v3);
 int q6asm_open_read_v4(struct audio_client *ac, uint32_t format,
 			uint16_t bits_per_sample, bool ts_mode)
 {
+#ifdef CONFIG_ARCH_MSM8996
+	return q6asm_open_read_v3(ac, format, bits_per_sample);
+#else
 	return __q6asm_open_read(ac, format, bits_per_sample,
 				 PCM_MEDIA_FORMAT_V4 /*media fmt block ver*/,
 				 ts_mode);
+#endif
 }
 EXPORT_SYMBOL(q6asm_open_read_v4);
 
@@ -3323,9 +3327,13 @@ EXPORT_SYMBOL(q6asm_open_write_v3);
 int q6asm_open_write_v4(struct audio_client *ac, uint32_t format,
 			uint16_t bits_per_sample)
 {
+#ifdef CONFIG_ARCH_MSM8996
+	return q6asm_open_write_v3(ac, format, bits_per_sample);
+#else
 	return __q6asm_open_write(ac, format, bits_per_sample,
 				  ac->stream_id, false /*gapless*/,
 				  PCM_MEDIA_FORMAT_V4 /*pcm_format_block_ver*/);
+#endif
 }
 EXPORT_SYMBOL(q6asm_open_write_v4);
 
@@ -3429,9 +3437,14 @@ int q6asm_stream_open_write_v4(struct audio_client *ac, uint32_t format,
 			       uint16_t bits_per_sample, int32_t stream_id,
 			       bool is_gapless_mode)
 {
+#ifdef CONFIG_ARCH_MSM8996
+	return q6asm_stream_open_write_v3(ac, format, bits_per_sample,
+					  stream_id, is_gapless_mode);
+#else
 	return __q6asm_open_write(ac, format, bits_per_sample,
 				  stream_id, is_gapless_mode,
 				  PCM_MEDIA_FORMAT_V4 /*pcm_format_block_ver*/);
+#endif
 }
 EXPORT_SYMBOL(q6asm_stream_open_write_v4);
 
@@ -4476,6 +4489,12 @@ fail_cmd:
 		return rc;
 }
 
+int q6asm_enc_cfg_blk_pcm_v3(struct audio_client *ac,
+			     uint32_t rate, uint32_t channels,
+			     uint16_t bits_per_sample, bool use_default_chmap,
+			     bool use_back_flavor, u8 *channel_map,
+			     uint16_t sample_word_size);
+
 /*
  * q6asm_enc_cfg_blk_pcm_v5 - sends encoder configuration parameters
  *
@@ -4604,6 +4623,12 @@ int q6asm_enc_cfg_blk_pcm_v4(struct audio_client *ac,
 	u8 *channel_mapping;
 	u32 frames_per_buf = 0;
 	int rc;
+
+#ifdef CONFIG_ARCH_MSM8996
+	return q6asm_enc_cfg_blk_pcm_v3(ac, rate, channels, bits_per_sample,
+					use_default_chmap, use_back_flavor,
+					channel_map, sample_word_size);
+#endif
 
 	if (!use_default_chmap && (channel_map == NULL)) {
 		pr_err("%s: No valid chan map and can't use default\n",
@@ -4868,6 +4893,7 @@ static int __q6asm_enc_cfg_blk_pcm_v5(struct audio_client *ac,
 					sample_word_size, endianness, mode);
 }
 
+#ifndef CONFIG_ARCH_MSM8996
 static int __q6asm_enc_cfg_blk_pcm_v4(struct audio_client *ac,
 				      uint32_t rate, uint32_t channels,
 				      uint16_t bits_per_sample,
@@ -4879,6 +4905,7 @@ static int __q6asm_enc_cfg_blk_pcm_v4(struct audio_client *ac,
 					bits_per_sample, true, false, NULL,
 					sample_word_size, endianness, mode);
 }
+#endif
 
 static int __q6asm_enc_cfg_blk_pcm_v3(struct audio_client *ac,
 				      uint32_t rate, uint32_t channels,
@@ -4948,9 +4975,14 @@ int q6asm_enc_cfg_blk_pcm_format_support_v4(struct audio_client *ac,
 					    uint16_t endianness,
 					    uint16_t mode)
 {
+#ifdef CONFIG_ARCH_MSM8996
+	 return __q6asm_enc_cfg_blk_pcm_v3(ac, rate, channels,
+					   bits_per_sample, sample_word_size);
+#else
 	 return __q6asm_enc_cfg_blk_pcm_v4(ac, rate, channels,
 					   bits_per_sample, sample_word_size,
 					   endianness, mode);
+#endif
 }
 EXPORT_SYMBOL(q6asm_enc_cfg_blk_pcm_format_support_v4);
 
@@ -5700,6 +5732,13 @@ static int __q6asm_media_format_block_pcm_v4(struct audio_client *ac,
 	u8 *channel_mapping;
 	int rc;
 
+#ifdef CONFIG_ARCH_MSM8996
+	return __q6asm_media_format_block_pcm_v3(ac, rate, channels,
+						 bits_per_sample, stream_id,
+						 use_default_chmap, channel_map,
+						 sample_word_size);
+#endif
+
 	pr_debug("%s: session[%d]rate[%d]ch[%d]bps[%d]wordsize[%d]\n", __func__,
 		 ac->session, rate, channels,
 		 bits_per_sample, sample_word_size);
@@ -6025,6 +6064,12 @@ static int __q6asm_media_format_block_multi_ch_pcm_v4(struct audio_client *ac,
 	struct asm_multi_channel_pcm_fmt_blk_param_v4 fmt;
 	u8 *channel_mapping;
 	int rc;
+
+#ifdef CONFIG_ARCH_MSM8996
+	return __q6asm_media_format_block_multi_ch_pcm_v3(ac, rate, channels,
+							  use_default_chmap, channel_map,
+							  bits_per_sample, sample_word_size);
+#endif
 
 	pr_debug("%s: session[%d]rate[%d]ch[%d]bps[%d]wordsize[%d]\n", __func__,
 		 ac->session, rate, channels,
