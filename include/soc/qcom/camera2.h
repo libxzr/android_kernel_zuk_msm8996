7c68e65dd80fdfde7ee7d7759bfc7b07fd9b9502
Subhajeet Muhuri <kenny3fcb@gmail.com>
2018-10-06 18:16:32 +0530

    camera: Squash reverts to make ZUK's camera working for newer CAF tags
    * Squash of:
    
    commit 9c909f4a62201b2ebb0ba56038d31bb85ae55d81
    Author: Subhajeet Muhuri <kenny3fcb@gmail.com>
    Date:   Sun Oct 28 14:47:09 2018 +0100
    
        Revert "msm: camera: Add laser LED support for depth sensor"
    
    	* "Camera error: Can't connect to the camera"
    
        This reverts commit 63f5792c5c6a8de2e40a9eee1195dfa264ff9431.
    
        Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    
    commit f7229d06853654871e970aa7bf604b01f38c3d37
    Author: Subhajeet Muhuri <kenny3fcb@gmail.com>
    Date:   Thu Oct 11 20:21:32 2018 +0530
    
        Revert "msm: camera: Bypass redundant creation of video node in stereo"
    
    	* Camera crashes upon launching
    
        This reverts commit f977e44aeaf5da07338a4649caa45367461226f0.
    
        Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    
    commit b0b964a9b6714ee4ff6af4e7f587f99921e3312a
    Author: YaroST12 <yaro330@gmail.com>
    Date:   Sat Oct 6 18:36:18 2018 +0530
    
        Revert "msm: cpp: Query buffer from buffer manager based on index"
    
    	* Breaks slow motion video recording
    
        This reverts commit ddff1a93bac277b1a0ba14c601d5236c3c36365d.
    
        Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    
    commit 5e1c739e4622428fa0b8fd9350946c31d2424b8e
    Author: Davide Garberi <dade.garberi@gmail.com>
    Date:   Sun Oct 28 14:32:43 2018 +0100
    
        Revert "msm: camera: Framedrop notification"
    
        This reverts commit 5925180360bca209c21e9271e277caaca68bcad5.
    
        Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    
    commit 0ef5dc40955cd7946f0a851feda516c34dc40d6d
    Author: YaroST12 <yaro330@gmail.com>
    Date:   Sat Oct 6 18:31:20 2018 +0530
    
        Revert "msm: camera: sensor: Adapting sensor driver to soc layer"
    
    	* Kernel panics at boot
    
        This reverts commit 6b99b4c9a1706f8f7f1745af97e0f4bddecf168b.
    
        Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    
    commit 1da8c59614f005d524455147fd78fc5f32cfa33b
    Author: Subhajeet Muhuri <kenny3fcb@gmail.com>
    Date:   Fri Oct 5 12:24:29 2018 +0530
    
        Revert "msm: camera: sensor: Updating soc layer clock API for i2c drivers"
    
    	* Required for the proper revert of 6b99b4c
    
        This reverts commit a4663702036e6b0ae9b82691027d9913d65d7a02.
    
        Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    
    commit 1b138397ea01ae70f653baf6c812fb5748b3783b
    Author: Subhajeet Muhuri <kenny3fcb@gmail.com>
    Date:   Sat Oct 6 18:19:19 2018 +0530
    
        Revert "msm: camera: Add regulator enable and disable independent of CSID"
    
        * Required for the proper revert of 6b99b4c
    
        This reverts commit fd39134867896ec5c28ce348816e31e7785e06ba.
    
        Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    
    commit 12e0c65509260092e8c3f22717080427a78c9469
    Author: Subhajeet Muhuri <kenny3fcb@gmail.com>
    Date:   Sat Oct 6 18:16:32 2018 +0530
    
        Revert "msm: camera: Add regulator enable and disable in csiphy"
    
        * Required for the proper revert of 6b99b4c
    
        This reverts commit d3e47e331617af3c87138bd205e65c6fa1a1bca7.
    
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    Change-Id: Iaa6aa22bb518b930141092fab9f51567c7eae092

--- include/soc/qcom/camera2.h
+++ include/soc/qcom/camera2.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -105,11 +105,10 @@ struct msm_camera_power_ctrl_t {
 	struct camera_vreg_t *cam_vreg;
 	int num_vreg;
 	struct msm_camera_i2c_conf *i2c_conf;
-	struct clk **clk_ptr;
 	struct msm_cam_clk_info *clk_info;
 	struct msm_pinctrl_info pinctrl_info;
 	uint8_t cam_pinctrl_status;
-	size_t clk_info_size;
+	uint16_t clk_info_size;
 };
 
 enum msm_camera_actuator_name {
