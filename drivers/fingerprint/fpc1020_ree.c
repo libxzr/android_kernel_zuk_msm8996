aa40b2357947186b2916edf70a142bcdb5d7f44a
Yaroslav Furman <yaro330@gmail.com>
2019-02-09 19:06:08 +0300

    fpc1020_ree: remove a memory barrier
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/fingerprint/fpc1020_ree.c
+++ drivers/fingerprint/fpc1020_ree.c
@@ -280,9 +280,6 @@ static irqreturn_t fpc1020_irq_handler(int irq, void *_fpc1020)
 	struct fpc1020_data *fpc1020 = _fpc1020;
 
 	pr_info("fpc1020 IRQ interrupt\n");
-	/* Make sure 'scree_on' is updated before using it
-	 ** since this is interrupt context (other thread...) */
-	smp_rmb();
 	if (!fpc1020->screen_on) {
 		pm_wakeup_event(fpc1020->dev, 5000);
 	}
