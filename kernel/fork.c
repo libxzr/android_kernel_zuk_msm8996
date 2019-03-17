22bf026e88f75eef64191d20f1c49cc5997a66fa
Yaroslav Furman <yaro330@gmail.com>
2019-03-18 00:21:39 +0300

    Revert "ANDROID: Fix massive cpufreq_times memory leaks"
    Sultan, [18.03.19 00:20]
    they fucked up my fix
    
    Sultan, [18.03.19 00:20]
    and made it leak in a new way
    
    Sultan, [18.03.19 00:20]
    dumbasses
    
    This reverts commit 1b37d68f4c82b1c40cc16478de1f8f6a1f584a82.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -78,10 +78,9 @@
 #include <linux/compiler.h>
 #include <linux/sysctl.h>
 #include <linux/kcov.h>
-#include <linux/cpufreq_times.h>
-#include <linux/cpufreq.h>
 #include <linux/cpu_input_boost.h>
 #include <linux/boost_control.h>
+#include <linux/cpufreq.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -232,7 +231,6 @@ static void account_kernel_stack(unsigned long *stack, int account)
 
 void free_task(struct task_struct *tsk)
 {
-	cpufreq_task_times_exit(tsk);
 	account_kernel_stack(tsk->stack, -1);
 	arch_release_thread_stack(tsk->stack);
 	free_thread_stack(tsk->stack);
@@ -1374,8 +1372,6 @@ static struct task_struct *copy_process(unsigned long clone_flags,
 	if (!p)
 		goto fork_out;
 
-	cpufreq_task_times_init(p);
-
 	/*
 	 * This _must_ happen before we call free_task(), i.e. before we jump
 	 * to any of the bad_fork_* labels. This is to avoid freeing
@@ -1828,8 +1824,6 @@ long _do_fork(unsigned long clone_flags,
 	if (!IS_ERR(p)) {
 		struct completion vfork;
 		struct pid *pid;
-
-		cpufreq_task_times_alloc(p);
 
 		trace_sched_process_fork(current, p);
 
