30f99fa9ec09a9c8aa14904d254b438bf8bdfbe8
LibXZR <xzr467706992@163.com>
2019-06-15 14:22:42 +0800

    Revert "zram: Do not allow compression algorithm to be changed"
    This reverts commit bd4eaed70f360f23042bf538d53e2e97897cf082.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -792,7 +792,6 @@ static ssize_t comp_algorithm_show(struct device *dev,
 static ssize_t comp_algorithm_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t len)
 {
-#if 0
 	struct zram *zram = dev_to_zram(dev);
 	char compressor[ARRAY_SIZE(zram->compressor)];
 	size_t sz;
@@ -815,7 +814,6 @@ static ssize_t comp_algorithm_store(struct device *dev,
 
 	strcpy(zram->compressor, compressor);
 	up_write(&zram->init_lock);
-#endif
 	return len;
 }
 
