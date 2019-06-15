e0b85bc4a2e3569b60c352b8e1f8f70048c44186
LibXZR <xzr467706992@163.com>
2019-06-15 14:21:58 +0800

    Squash remove z3fold
    * Also re-enable zram and move back to zmalloc

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -16,26 +16,10 @@
 #define _ZRAM_DRV_H_
 
 #include <linux/rwsem.h>
-#include <linux/zpool.h>
+#include <linux/zsmalloc.h>
 #include <linux/crypto.h>
 
 #include "zcomp.h"
-
-/*-- Configurable parameters */
-
-/*
- * Pages that compress to size greater than this are stored
- * uncompressed in memory.
- */
-static const size_t max_zpage_size = PAGE_SIZE / 4 * 3;
-
-/*
- * NOTE: max_zpage_size must be less than or equal to:
- *   ZS_MAX_ALLOC_SIZE. Otherwise, zs_malloc() would
- * always return failure.
- */
-
-/*-- End of configurable params */
 
 #define SECTOR_SHIFT		9
 #define SECTORS_PER_PAGE_SHIFT	(PAGE_SHIFT - SECTOR_SHIFT)
@@ -100,7 +84,7 @@ struct zram_stats {
 
 struct zram {
 	struct zram_table_entry *table;
-	struct zpool *mem_pool;
+	struct zs_pool *mem_pool;
 	struct zcomp *comp;
 	struct gendisk *disk;
 	/* Prevent concurrent execution of device init */
