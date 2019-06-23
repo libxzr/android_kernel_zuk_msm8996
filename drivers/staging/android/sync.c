c6920e425b1fcead375ee6f66d713b0020a9ebb7
Sultan Alsawaf <sultan@kerneltoast.com>
2019-06-23 07:35:15 -0700

    Revert "staging: sync: Don't allocate memory dynamically for fence info"
    Uh-oh, this ioctl does run in parallel/concurrently. This causes system
    freezes.
    
    This reverts commit f9dcff6db6cf3f3cfb87db882c2118961067aa49.

--- drivers/staging/android/sync.c
+++ drivers/staging/android/sync.c
@@ -668,8 +668,7 @@ static int sync_fill_pt_info(struct fence *fence, void *data, int size)
 static long sync_fence_ioctl_fence_info(struct sync_fence *fence,
 					unsigned long arg)
 {
-	static u8 data_buf[4096] ____cacheline_aligned_in_smp;
-	struct sync_fence_info_data *data = (typeof(data))data_buf;
+	struct sync_fence_info_data *data;
 	__u32 size;
 	__u32 len = 0;
 	int ret, i;
@@ -682,6 +681,10 @@ static long sync_fence_ioctl_fence_info(struct sync_fence *fence,
 
 	if (size > 4096)
 		size = 4096;
+
+	data = kzalloc(size, GFP_KERNEL);
+	if (data == NULL)
+		return -ENOMEM;
 
 	strlcpy(data->name, fence->name, sizeof(data->name));
 	data->status = atomic_read(&fence->status);
@@ -696,7 +699,7 @@ static long sync_fence_ioctl_fence_info(struct sync_fence *fence,
 		ret = sync_fill_pt_info(pt, (u8 *)data + len, size - len);
 
 		if (ret < 0)
-			return ret;
+			goto out;
 
 		len += ret;
 	}
@@ -707,6 +710,9 @@ static long sync_fence_ioctl_fence_info(struct sync_fence *fence,
 		ret = -EFAULT;
 	else
 		ret = 0;
+
+out:
+	kfree(data);
 
 	return ret;
 }
