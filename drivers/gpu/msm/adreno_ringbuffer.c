bdb91bdc3cd2455a8090ca242716f9fb2ce52623
LibXZR <xzr467706992@163.com>
2019-08-04 21:36:21 +0800

    Revert "msm: kgsl: Avoid dynamically allocating small command buffers"
    This reverts commit 6b6d3f0ffb850e9ddb1897da9f8598d899f1cb5a.

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -748,7 +748,6 @@ int adreno_ringbuffer_submitcmd(struct adreno_device *adreno_dev,
 	struct kgsl_memobj_node *ib;
 	unsigned int numibs = 0;
 	unsigned int *link;
-	unsigned int link_onstack[SZ_256];
 	unsigned int *cmds;
 	struct kgsl_context *context;
 	struct adreno_context *drawctxt;
@@ -868,14 +867,10 @@ int adreno_ringbuffer_submitcmd(struct adreno_device *adreno_dev,
 				adreno_is_preemption_enabled(adreno_dev))
 		dwords += 8;
 
-	if (dwords <= ARRAY_SIZE(link_onstack)) {
-		link = link_onstack;
-	} else {
-		link = kmalloc(sizeof(unsigned int) * dwords, GFP_KERNEL);
-		if (!link) {
-			ret = -ENOMEM;
-			goto done;
-		}
+	link = kzalloc(sizeof(unsigned int) *  dwords, GFP_KERNEL);
+	if (!link) {
+		ret = -ENOMEM;
+		goto done;
 	}
 
 	cmds = link;
@@ -1027,8 +1022,7 @@ done:
 	trace_kgsl_issueibcmds(device, context->id, numibs, drawobj->timestamp,
 			drawobj->flags, ret, drawctxt->type);
 
-	if (link != link_onstack)
-		kfree(link);
+	kfree(link);
 	return ret;
 }
 
