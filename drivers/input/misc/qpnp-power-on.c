6f84aace62c9671b7b6c681ba7f32cbbb6faa304
Julian Liu <wlootlxt123@gmail.com>
2020-03-24 23:40:33 +0800

    qpnp-power-on: Do fsync during power reseting
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>
    
    * This enables sync when long press power button, avoid data loss caused by force reboot.
    
    libxzr: Adjust k4.4
    
    Signed-off-by: LibXZR <xzr467706992@163.com>

--- drivers/input/misc/qpnp-power-on.c
+++ drivers/input/misc/qpnp-power-on.c
@@ -32,6 +32,7 @@
 #include <linux/regulator/of_regulator.h>
 #include <linux/input/qpnp-power-on.h>
 #include <linux/power_supply.h>
+#include <linux/syscalls.h>
 
 #define PMIC_VER_8941           0x01
 #define PMIC_VERSION_REG        0x0105
@@ -155,6 +156,9 @@
 
 #define QPNP_POFF_REASON_UVLO			13
 
+#define FS_SYNC_INTERVAL_MS			500
+#define FS_SYNC_MAX_TIMERS 			8
+
 enum qpnp_pon_version {
 	QPNP_PON_GEN1_V1,
 	QPNP_PON_GEN1_V2,
@@ -221,6 +225,10 @@ struct qpnp_pon {
 	bool			store_hard_reset_reason;
 	bool			kpdpwr_dbc_enable;
 	ktime_t			kpdpwr_last_release_time;
+
+	struct delayed_work	fsync_timer;
+	spinlock_t		fs_sync_lock;
+	u8			fs_sync_counter;
 };
 
 static int pon_ship_mode_en;
@@ -793,6 +801,43 @@ qpnp_get_cfg(struct qpnp_pon *pon, u32 pon_type)
 	return NULL;
 }
 
+static void fs_sync_func(struct work_struct *work)
+{
+	struct qpnp_pon *pon = sys_reset_dev;
+	int rc;
+	uint pon_rt_sts;
+	u8 sync_counter;
+
+	if (pon->fs_sync_counter == 0)
+	{
+		rc = regmap_read(pon->regmap, QPNP_PON_RT_STS(pon), &pon_rt_sts);
+		if (rc) {
+			dev_err(&pon->pdev->dev, "Unable to read PON RT status\n");
+			return;
+		}
+		rc = !(pon_rt_sts & QPNP_PON_KPDPWR_N_SET) && !(pon_rt_sts & QPNP_PON_KPDPWR_RESIN_BARK_N_SET);
+		if (rc)
+			return;
+
+		dev_info(&pon->pdev->dev, "Syncing filesystem\n");
+	}
+
+	sys_sync();
+
+	spin_lock_irq(&pon->fs_sync_lock);
+	sync_counter = pon->fs_sync_counter++;
+	if (sync_counter < FS_SYNC_MAX_TIMERS - 1)
+	{
+		spin_unlock_irq(&pon->fs_sync_lock);
+		schedule_delayed_work(&pon->fsync_timer, msecs_to_jiffies(FS_SYNC_INTERVAL_MS));
+	}
+	else
+	{
+		pon->fs_sync_counter = 0;
+		spin_unlock_irq(&pon->fs_sync_lock);
+	}
+}
+
 static int
 qpnp_pon_input_dispatch(struct qpnp_pon *pon, u32 pon_type)
 {
@@ -860,6 +905,15 @@ qpnp_pon_input_dispatch(struct qpnp_pon *pon, u32 pon_type)
 	if (!cfg->old_state && !key_status) {
 		input_report_key(pon->pon_input, cfg->key_code, 1);
 		input_sync(pon->pon_input);
+	}
+
+	if ((cfg->pon_type == PON_KPDPWR || cfg->pon_type == PON_KPDPWR_RESIN)
+ 	&& key_status)
+	{
+		spin_lock(&pon->fs_sync_lock);
+		pon->fs_sync_counter = 0;
+		spin_unlock(&pon->fs_sync_lock);
+		schedule_delayed_work(&pon->fsync_timer, msecs_to_jiffies(4*FS_SYNC_INTERVAL_MS));
 	}
 
 	input_report_key(pon->pon_input, cfg->key_code, key_status);
@@ -2360,6 +2414,11 @@ static int qpnp_pon_probe(struct platform_device *pdev)
 					"qcom,store-hard-reset-reason");
 
 	qpnp_pon_debugfs_init(pdev);
+
+	pon->fs_sync_counter = 0;
+	spin_lock_init(&pon->fs_sync_lock);
+	INIT_DELAYED_WORK(&pon->fsync_timer, fs_sync_func);
+
 	return 0;
 }
 
