4bffa3e82906c0f166a5f603e99b5f213abdd76d
LibXZR <xzr467706992@163.com>
2020-04-04 20:12:17 +0800

    msm-poweroff: Do sys_sync() on restart & poweroff
    * Kang from xiaomi panic changes

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -25,6 +25,8 @@
 #include <linux/delay.h>
 #include <linux/input/qpnp-power-on.h>
 #include <linux/of_address.h>
+#include <linux/syscalls.h>
+#include <linux/workqueue.h>
 
 #include <asm/cacheflush.h>
 #include <asm/system_misc.h>
@@ -400,9 +402,31 @@ static void deassert_ps_hold(void)
 	__raw_writel(0, msm_ps_hold);
 }
 
+#define FS_SYNC_TIMEOUT_MS 5000
+static struct work_struct fs_sync_work;
+static DECLARE_COMPLETION(sync_compl);
+
+static void fs_sync_work_func(struct work_struct *work)
+{
+	pr_emerg("sys_sync: Syncing fs\n");
+	sys_sync();
+	complete(&sync_compl);
+}
+
+void exec_fs_sync_work(void)
+{
+	INIT_WORK(&fs_sync_work, fs_sync_work_func);
+	reinit_completion(&sync_compl);
+	schedule_work(&fs_sync_work);
+	if (wait_for_completion_timeout(&sync_compl, msecs_to_jiffies(FS_SYNC_TIMEOUT_MS)) == 0)
+		pr_emerg("sys_sync: Timeout\n");
+}
+
 static void do_msm_restart(enum reboot_mode reboot_mode, const char *cmd)
 {
 	pr_notice("Going down for restart now\n");
+
+	exec_fs_sync_work();
 
 	msm_restart_prepare(cmd);
 
@@ -426,6 +450,8 @@ static void do_msm_restart(enum reboot_mode reboot_mode, const char *cmd)
 static void do_msm_poweroff(void)
 {
 	pr_notice("Powering off the SoC\n");
+
+	exec_fs_sync_work();
 
 	set_dload_mode(0);
 	scm_disable_sdi();
